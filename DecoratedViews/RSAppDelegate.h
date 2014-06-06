204621b111906aa927d95d6e63d9442f48a247e5
Mark Williams <markwilliams@Marks-MacBook-Pro.local>
2014-06-06 11:54:50 +0100

    Initial Commit

---
+++ DecoratedViews/RSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RSAppDelegate.h
+//  DecoratedViews
+//
+//  Created by Mark Williams on 06/06/2014.
+//  Copyright (c) 2014 Mark Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
